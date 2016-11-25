0ad8f7a566b66e63c54bd8951d16c772a85121c9
bit-user <bit-user@bit>
2016-11-25 14:17:19 +0900

    text 표시

--- app/src/main/java/com/bit2016/datepickerexample/MainActivity.java
+++ app/src/main/java/com/bit2016/datepickerexample/MainActivity.java
@@ -2,7 +2,14 @@ package com.bit2016.datepickerexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private DatePicker datePicker;
@@ -13,5 +20,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         datePicker = (DatePicker) findViewById(R.id.datePicker);
+        Calendar calendar = Calendar.getInstance();
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int date = calendar.get(Calendar.DATE);
+        datePicker.init(year, month, date, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                String date = year + "-" + (monthOfYear+1) + "-" + dayOfMonth;
+
+                ((TextView)findViewById(R.id.dateText)).setText(date);
+            }
+        });
+
+                findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int year = datePicker.getYear();
+                        int month = datePicker.getMonth();
+                        int date = datePicker.getDayOfMonth();
+
+                        String dateSelectd = year + "년" + month + "월" + date + "일";
+                        Toast.makeText(MainActivity.this , dateSelectd, Toast.LENGTH_LONG).show();
+                    }
+                });
+
     }
 }
